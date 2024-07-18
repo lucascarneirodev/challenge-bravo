ae303167ad8172b8cd018144983f99edffe3f4eb
joaodath <60579550+lucascarneirodev@users.noreply.github.com>
2024-07-18 20:37:30 -0300

    wip: currency_converter endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from typing import Union
+from typing import Union, Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -8,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# Endpoint converting a currency to another
+@app.get("/convert/")
+async def currency_converter(currency_from: Annotated[str | None, Query(alias="from")] = None, currency_to: Annotated[str | None, Query(alias="to")] = None, amount: float = 1.00):
+    return {
+        "from": f'{currency_from}',
+        "to": f'{currency_to}',
+        "amount": f'{amount}'
+    }
