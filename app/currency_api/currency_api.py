e6030d4c6c4960a7aba65527aa916a6203aedd5b
joaodath <60579550+lucascarneirodev@users.noreply.github.com>
2024-08-01 22:08:53 -0300

    fix: do not raise errors if .env not found (docker container do not import .env)

--- app/currency_api/currency_api.py
+++ app/currency_api/currency_api.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import currencyapicom
 
-load_dotenv(find_dotenv(raise_error_if_not_found=True))
+load_dotenv(find_dotenv(raise_error_if_not_found=False))
 
 currency_api_client = currencyapicom.Client(os.environ["CURRENCY_API_KEY2"])
 
