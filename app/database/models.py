a7d21675709b68b3cb770d2776344a1a2bcc1955
lucascarneirodev <60579550+lucascarneirodev@users.noreply.github.com>
2024-09-20 22:09:17 -0300

    fix: pylance warning for deprecated method datetime.utcnow

--- app/database/models.py
+++ app/database/models.py
@@ -3,7 +3,7 @@ from typing import List
 from decimal import Decimal
 from sqlalchemy import Column, Integer, String, ForeignKey, JSON, DECIMAL, UniqueConstraint
 from sqlalchemy.dialects.mysql import VARCHAR
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 class Currency(SQLModel, table=True):
   id: int | None = Field(sa_column=Column(Integer, primary_key=True, autoincrement=True))
@@ -24,6 +24,6 @@ class CurrencyExchange(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
   currency_code: str = Field(sa_column=Column(String(255), ForeignKey("currency.code")))
   value: Decimal = Field(sa_column=Column(DECIMAL(21, 5), nullable=False))
-  date: datetime = Field(default_factory=datetime.utcnow)
+  date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
