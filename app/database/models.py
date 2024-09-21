18ee6d9e613900d810e9bb953850d88f66bbf1ec
lucascarneirodev <60579550+lucascarneirodev@users.noreply.github.com>
2024-09-20 22:05:30 -0300

    fix: update data modelling

--- app/database/models.py
+++ app/database/models.py
@@ -1,17 +1,17 @@
 from sqlmodel import Field, SQLModel, create_engine
 from typing import List
 from decimal import Decimal
-from sqlalchemy import Column, Integer, String, ForeignKey, JSON,DECIMAL
+from sqlalchemy import Column, Integer, String, ForeignKey, JSON, DECIMAL, UniqueConstraint
 from sqlalchemy.dialects.mysql import VARCHAR
 from datetime import datetime, timedelta
 
 class Currency(SQLModel, table=True):
   id: int | None = Field(sa_column=Column(Integer, primary_key=True, autoincrement=True))
-  symbol: str = Field(max_length=255, primary_key=True)
+  symbol: str = Field(max_length=255)
   symbol_native: str = Field(max_length=255)
   decimal_digits: int
   rounding: int
-  code: str = Field(max_length=255, primary_key=True)
+  code: str = Field(sa_column=Column(String(255), unique=True, nullable=False))
   name: str = Field(max_length=255)
   name_plural: str = Field(max_length=255)
   currency_type: str = Field(max_length=255)
@@ -22,8 +22,7 @@ class Currency(SQLModel, table=True):
 
 class CurrencyExchange(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
-  base_currency_code: str = Field(sa_column=Column(String(255), ForeignKey("currency.code")))
-  foreign_currency_code: str = Field(sa_column=Column(String(255), ForeignKey("currency.code")))
+  currency_code: str = Field(sa_column=Column(String(255), ForeignKey("currency.code")))
   value: Decimal = Field(sa_column=Column(DECIMAL(21, 5), nullable=False))
   date: datetime = Field(default_factory=datetime.utcnow)
 
